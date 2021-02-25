bae465284ab3da32eeef0a732cd18628d81a1d68
hridoy05 <hridoy.banik71@gmail.com>
2021-02-25 19:15:39 +0600

    deployment procedure

--- Blog_Project/settings.py
+++ Blog_Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,7 +49,6 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,7 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'staticfiles'
 STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Media
@@ -135,3 +134,5 @@ MEDIA_URL = '/media/'
 
 
 LOGIN_URL = '/account/login/'
+
+django_heroku.settings(locals())
